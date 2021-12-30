99746f46d052aa45615ddfbe8228366d35e66579
harun7arrashid <harun4arrashid@gmail.com>
2021-12-30 07:18:45 +0700

    Bottom Navigation Fragment

--- app/src/main/java/id/harun/cloneklicknclean/MainActivity.kt
+++ app/src/main/java/id/harun/cloneklicknclean/MainActivity.kt
@@ -2,11 +2,38 @@ package id.harun.cloneklicknclean
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import id.harun.cloneklicknclean.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navView: BottomNavigationView = binding.bottomNav
+        val navController: NavController  = findNavController(R.id.nav_host)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home,
+            R.id.navigation_order,
+            R.id.navigation_package,
+            R.id.navigation_prepaid,
+            R.id.navigation_account
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
